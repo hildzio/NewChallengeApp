a6a70814d4fb301827dcfdb82526e8200fbe4ea1
Hildek <tomasz.hildebrand@gmail.com>
2023-01-21 21:36:11 +0100

    Zadanie domowe po dniu 5

--- NewChallengeApp/NewChallengeApp/Program.cs
+++ NewChallengeApp/NewChallengeApp/Program.cs
@@ -1,13 +1,27 @@
 ﻿Console.WriteLine("Podaj liczbę do analizy: ");
-string numberInString = Console.ReadLine();
-char[] letters = numberInString.ToArray();
-List<string> result = new List<string>();
+string digitInString = Console.ReadLine();
+char[] digitsFromInput = digitInString.ToArray();
+List<string> digitsFromZeroToNine = new();
+List<int> occurDigits = new();
+int digit = 0;
 
-for (var i = 0; i < letters.Length; i++)
+for (int n = 0; n <= 9; n++)
 {
-    if (letters[i] == 0)
+    string nString = digit.ToString();
+    digitsFromZeroToNine.Add(nString);
+    digit++;
+}
+Console.WriteLine($"Wyniki dla liczby : {digitInString}");
+
+for (var j = 0; j < digitsFromZeroToNine.Count; j++)
+{
+    var countOccurDigits = 0;
+    for (var i = 0; i < digitsFromInput.Length; i++)
     {
-
+        if (digitsFromInput[i].ToString() == digitsFromZeroToNine[j])
+        {
+            countOccurDigits++;
+        }
     }
-
+    Console.WriteLine($"{digitsFromZeroToNine[j]} => {countOccurDigits}");
 }
