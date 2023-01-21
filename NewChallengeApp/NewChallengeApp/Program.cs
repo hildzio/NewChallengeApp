667b61c69af9e27e1e31f47ce2449c55f0e6394a
Hildek <tomasz.hildebrand@gmail.com>
2023-01-21 17:42:09 +0100

    zmiana folderu

---
+++ NewChallengeApp/NewChallengeApp/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Podaj liczbę do analizy: ");
+string numberInString = Console.ReadLine();
+char[] letters = numberInString.ToArray();
+List<string> result = new List<string>();
+
+for (var i = 0; i < letters.Length; i++)
+{
+    if (letters[i] == 0)
+    {
+
+    }
+
+}
