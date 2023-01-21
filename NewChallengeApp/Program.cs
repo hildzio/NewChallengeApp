d10b4a2aed1f3a385f4494aeddeb3b42657ce7c1
Hildek <tomasz.hildebrand@gmail.com>
2023-01-21 01:10:55 +0100

    Zadanie domowe dzień 4

--- NewChallengeApp/Program.cs
+++ NewChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+char sex = 'F';
+int age = 30;
+
+if (sex=='f' && age <30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age <18 && sex =='M')
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (sex == 'M')
+{
+    Console.WriteLine($"Naszym bohaterem jest mężczyzna {name} w wieku {age} lat.");
+}
+else
+{
+    Console.WriteLine($"Naszą bohaterką jest kobieta {name} w wieku {age} lat.");
+}
